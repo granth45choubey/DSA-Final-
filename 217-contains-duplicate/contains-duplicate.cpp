05fd2e70573120e9ee7d9a5970b67b0d2ed0ad37
Granth Choubey <153370188+granth45choubey@users.noreply.github.com>
2026-07-06 00:33:20 +0530

    Time: 99 ms (10.56%) | Memory: 113.5 MB (6.33%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -4,9 +4,9 @@ public:
       
         unordered_map<int,int> mp;
 
-        for(int num : nums){
-            mp[num]++;
-        }
+       for(int i = 0; i < nums.size(); i++){
+                 mp[nums[i]]++;
+                }
 
         if(mp.size() == nums.size())
             return false;
